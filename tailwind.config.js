bb5f2643ca894d1b62bd79221e501c439d4c5bf7
Kuberan002 <kuberansivakumar002@gmail.com>
2025-08-12 15:38:18 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // This is an example. Make sure your paths are correct.
+    "./*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
